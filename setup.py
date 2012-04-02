243d894f7c9a77cf995fb4a6fc7263f82f53a138
vsirisanthana <vsirisanthana@gmail.com>
2012-04-02 10:38:26 +0000

    Upgrade to version 0.0.6

--- setup.py
+++ setup.py
@@ -7,20 +7,22 @@ def read(fname):
 
 setup(
     name = "django-serene",
-    version = "0.0.5",
+    version = "0.0.6",
     author = "The Sirisanthana Team",
     author_email = "vsirisanthana@gmail.com",
-    description = ("An enhanced djangorestframework. Serene is a RESTful framework with a touch of sirisanthana"),
+    url = "http://code.google.com/p/django-serene/",
+    description = "An enhanced djangorestframework. Serene is a RESTful framework with a touch of sirisanthana",
     long_description = read('README.txt'),
-    license = "GPL-3.0",
-    keywords = "RESTful django djangorestframework",
+    download_url = "http://pypi.python.org/pypi/django-serene",
     classifiers = [
         "Development Status :: 3 - Alpha",
         "Intended Audience :: Developers",
         "Programming Language :: Python",
         "Topic :: Software Development :: Libraries :: Python Modules",
         "License :: OSI Approved :: GNU General Public License (GPL)"
-        ],
+    ],
+    license = "GPL-3.0",
+    keywords = "RESTful django djangorestframework",
     packages = ['serene', 'serene.tests'],
     install_requires = ['djangorestframework'],
 )
